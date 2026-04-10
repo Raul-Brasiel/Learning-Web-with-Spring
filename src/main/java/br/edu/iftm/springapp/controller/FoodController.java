eed866bc202c0d1ad640ff27d2e7090390f147dd
Raul-Brasiel <raulbrasiel65@gmail.com>
2026-04-10 20:45:44 -0300

    Adição da funcionalidade de cadastrar produto

--- src/main/java/br/edu/iftm/springapp/controller/FoodController.java
+++ src/main/java/br/edu/iftm/springapp/controller/FoodController.java
@@ -9,4 +9,16 @@ public class FoodController{
         model.addAttribute("foodsList", foodService.getAllFoods());
         return "food/index";
     }
+
+    @GetMapping("/food/create")
+    public String create(Model model){
+        model.addAttribute("food", new Food());
+        return "food/create";
+    }
+
+    @GetMapping("/food/save")
+    public String save(@ModelAttribute("food") Food food){
+        foodService.saveFood(food);
+        return "redirect:/food";
+    }
 }
